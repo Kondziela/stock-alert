f29cc50f02ac4448a0d2705da459081158cb9d68
AKondziela <artur.kondziela@gmail.com>
2019-09-12 00:54:07 +0200

    Repair import

--- src/request.js
+++ src/request.js
@@ -4,7 +4,7 @@
  */
 
 var request = require('request-promise'),
-	token = require('../token.js').token;
+	token = require('./token.js').token;
 
 
 var	BASE_URL = 'https://api.tiingo.com/tiingo/daily/',
