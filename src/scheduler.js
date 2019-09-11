85ccf70ef35dadc47ba87689f1c8505ffa35f42c
AKondziela <artur.kondziela@gmail.com>
2019-09-12 00:26:41 +0200

    Repair scheduler

--- src/scheduler.js
+++ src/scheduler.js
@@ -2,6 +2,6 @@ var schedule = require('node-schedule'),
 	main = require('./main');
  
 // https://www.npmjs.com/package/node-schedule
-var j = schedule.scheduleJob('5 * * * * *'/*'0 0 23 * * *'*/, function(){
+var j = schedule.scheduleJob('0 0 23 * * *', function(){
   main.mainProcess();
 });
