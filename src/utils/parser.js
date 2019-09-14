3c4bc45dd00a1a0e927e0af81b03e2e0ec0dfce4
AKondziela <artur.kondziela@gmail.com>
2019-09-14 12:47:02 +0200

    Repair after master merge part 2

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,10 +1,10 @@
 var parseTiingoResponse = (body) => 
 	JSON.parse(body).map( object => {return {
 		'date': object.date,
-		'close': object.close
-		// 'high': object.high,
-		// 'low': object.low,
-		// 'open': object.open
+		'close': object.close,
+		'high': object.high,
+		'low': object.low,
+		'open': object.open
 	}});
 
 module.exports.parseTiingoResponse = parseTiingoResponse;
