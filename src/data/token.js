4fd87ff015d193e306464a7fdbe7a1e7719483fd
AKondziela <artur.kondziela@gmail.com>
2019-09-14 18:39:37 +0200

    Remove unused value

--- src/data/token.js
+++ src/data/token.js
@@ -1,8 +1,5 @@
 // tiingi
 module.exports.token = "91a345dff31524acd5a8097bfe840823208b9de7";
 
-// messengeer
-module.exports.app_token = "f15569f4a3e263303916f41b2dec263d";
-
 // slack
 module.exports.slack_webhooks = "https://hooks.slack.com/services/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";
