e8d5077ef2479387d1093bd92a08f9d510e1561c
Kondziela <artur.kondziela@gmail.com>
2019-11-14 20:13:47 +0100

    Refactor js to ts for senders

--- src/senders/request.ts
+++ src/senders/request.ts
@@ -8,37 +8,39 @@
  *  - Yahoo https://rapidapi.com/apidojo/api/yahoo-finance1/
  */
 
-var request = require('request-promise');
+import * as request from 'request-promise';
 
+export class Request {
 
-var	BASE_URL = 'https://api.tiingo.com/tiingo/daily/',
-	requestOptions = {
+    private TIINGO_URL: string = 'https://api.tiingo.com/tiingo/daily/';
+    private QUANDL_URL: string = 'https://www.quandl.com/api/v3/datasets/FSE/';
+    private AUTHENTICATION : object = {
         'headers': {
             'Content-Type': 'application/json',
             'Authorization': 'Token ' + process.env.tiingi_token
             }
-        },
+        }
 
-    requestForUSAStock = (company, startDate) => {
+    public requestForUSAStock(company: string, startDate: string): Promise<string> {
     	if (!company || !startDate) {
     		console.log("Company: " + company);
     		console.log("Start date: " + startDate);
     		throw new Error("Missing param");
     	}
-    	let url = BASE_URL + company + '/prices?startDate=' + startDate;
-    	requestOptions.url = url; 
+        let url = `${this.TIINGO_URL}${company}/prices?startDate=${startDate}`,
+            requestOptions = {...this.AUTHENTICATION};
+        requestOptions['url'] = url; 
+        
     	return request(requestOptions);
-    },
+    }
 
-    requestForGermanStock = (company, startDate) => {
+    requestForGermanStock(company: string, startDate: string): Promise<string> {
         var options = {
           method: 'GET',
-          url: `https://www.quandl.com/api/v3/datasets/FSE/${company}?start_date=${startDate}&api_key=${process.env.german_token}`
+          url: `${this.QUANDL_URL}${company}?start_date=${startDate}&api_key=${process.env.german_token}`
         };
 
         console.log(`Request for german stock: ${options.url}`);
         return request(options);
     }
-
-module.exports.requestForUSAStock = requestForUSAStock;
-module.exports.requestForGermanStock = requestForGermanStock;
+}
