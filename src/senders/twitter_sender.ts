c4ede4c633595b9396e5baa426b8fba01d7be49f
Kondziela <artur.kondziela@gmail.com>
2019-11-25 22:31:51 +0100

    Convert tweets and use sentiment analyze

--- src/senders/twitter_sender.ts
+++ src/senders/twitter_sender.ts
@@ -1,14 +1,22 @@
 import * as Twit from 'twit';
 import {Util} from "../utils/util";
+import * as Sentiment from 'sentiment';
 
 export class TwitterSender {
 
     private client: Twit;
     private util: Util;
+    private sentiment: Sentiment;
+    private companyMap: Object;
 
     constructor() {
         this.util = new Util();
-        console.log(`Consumer key ${process.env.consumer_key}`);
+        this.sentiment = new Sentiment();
+        this.companyMap = {
+            Facebook: ['facebook'],
+            Apple: ['apple', 'iphone'],
+            Amazon: ['amazon', 'aws']
+        };
         this.client = new Twit({
             consumer_key: process.env.consumer_key,
             consumer_secret: process.env.consumer_secret,
@@ -18,20 +26,58 @@ export class TwitterSender {
     }
 
     public createStream() {
-        let stream = this.client.stream('statuses/filter', { track: ['#cdr', '@cdr', '#amazon', '#AWS'], lang: 'en', tweet_mode: 'full_text'}),
+        let stream = this.client.stream('statuses/filter', { 
+            track: ['#facebook', '#apple', '#amazon', '#AWS'], 
+            language: 'en', 
+            tweet_mode: 'extended'
+        }),
             me = this;
 
         console.log('Creating stream...');
-        stream.on('tweet', function (tweet) {
-            console.log(me.convertTweet(tweet));
+        stream.on('tweet', function (originalTweet) {
+            let tweet = me.convertTweet(originalTweet);
+            console.log(tweet);
+            let companies = me.findCompanies([...tweet['hashtags']]);
+            console.log('Companies: ', companies);
+            let analyze = me.sentiment.analyze(tweet['text']);
+            console.log('Analyze of sentiment', analyze['score']);
         });
         stream.on('connect', function (requst) {
              console.log('Connect');
         });
         stream.on('reconnect', function (request, response, connectInterval) {
-             console.log('reconnect');
+             console.log('Reconnect');
         });
     }
+
+    private findCompanies(hashtagList): Array<String> {
+        let me = this;
+        return Object.keys(this.companyMap)
+            .filter( (company) => {
+                return hashtagList.some(hashtag => this.companyMap[company].includes(hashtag.toLowerCase()))
+            });
+    }
+
+    private convertTweet(tweet: Object): {date: Date, text: String, id: number, hashtags: Array<String>, user_mentions: Array<String>} {
+        let extendedTweet = tweet['retweeted_status'] ? tweet['retweeted_status']['extended_tweet'] : tweet['extended_tweet'],
+            text = tweet['truncated'] ? extendedTweet['full_text'] : tweet['text'];
+
+        if (!extendedTweet && tweet['truncated'] ) {
+            console.log(tweet);
+            throw new Error('Unhandle tweet');
+        }
+
+        return {
+            date: new Date(tweet['created_at']),
+            text: text,
+            id: tweet['id_str'],
+            hashtags: tweet['entities']['hashtags'].map( hashtag => hashtag['text']),
+            user_mentions: tweet['entities']['user_mentions'].map( user => user['name'])
+        }
+    }
+
+    // SEARCH FUNCTION
+    // NOT USE IN APPLICATION
 
     public search(searchConfig: Object): Promise<any> {
         searchConfig['count'] = 100;
@@ -78,21 +124,6 @@ export class TwitterSender {
 
     private convertTweets(tweets: Object): Array<Object> {
         return tweets['statuses'].map( tweet => this.convertTweet(tweet));
-    }
-
-    private convertTweet(tweet: Object): Object {
-        let extendedTweet = tweet['extended_tweet'];
-
-        console.log(tweet);
-
-        return {
-            date: new Date(tweet['created_at']),
-            text: tweet['text'],
-            id: tweet['id_str'],
-            hashtags: tweet['entities']['hashtags'],
-            user_mentions: tweet['entities']['user_mentions'].map( user => user['name']),
-            full_text: tweet['full_text']
-        };
     }
 
     private decreaseByOne(id: String): String {
--- src/test_start.ts
+++ src/test_start.ts
@@ -5,8 +5,6 @@ import {TwitterSender} from "./senders/twitter_sender";
  * Sandbox class for testing scripts. Feel free to adjust.
  */
 new MainBot().initEnvironmentVariables().then(() => {
-    new TwitterSender().search({ q: '@CDR', lang: 'en'}).then( tweets => {
-        console.log(tweets);
-    });
+    new TwitterSender().createStream();
 });
 
