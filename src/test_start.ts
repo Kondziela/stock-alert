9a2ca3cc8da2f0b8aaeba5dee5739f5e8a04d53b
Artur Kondziela <artur.kondziela@gmail.com>
2019-11-28 14:58:02 +0100

    Comment no used code

--- src/test_start.ts
+++ src/test_start.ts
@@ -1,10 +1,10 @@
-import {MainBot} from "./main_bot";
-import {TwitterSender} from "./senders/twitter_sender";
+// import {MainBot} from "./main_bot";
+// import {TwitterSender} from "./senders/twitter_sender";
 
-/**
- * Sandbox class for testing scripts. Feel free to adjust.
- */
-new MainBot().initEnvironmentVariables().then(() => {
-    new TwitterSender().createStream();
-});
+// /**
+//  * Sandbox class for testing scripts. Feel free to adjust.
+//  */
+// new MainBot().initEnvironmentVariables().then(() => {
+//     new TwitterSender().createStream();
+// });
 
